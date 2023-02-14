4763ea04665acd51239d71ad11b59539e471442a
LcsCamacho <lucas.camachofilhoo@gmail.com>
2023-02-14 09:36:40 -0300

    allow cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "http://127.0.0.1:5500/delivery/" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ]
+      }
+    ]
+  },
   reactStrictMode: true,
 }
 
